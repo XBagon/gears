c07dd8080e9291c84bd345d1e1606b7402118892
XBagon <xbagon@outlook.de>
2022-01-05 17:29:27 +0100

    [skip ci] delete commented out old test

--- gears_core/src/lib.rs
+++ gears_core/src/lib.rs
@@ -9,19 +9,6 @@ mod tests {
     use crate::gear::compound::GearCompound;
     use crate::gear::*;
 
-    /*
-    #[test]
-    fn it_works() {
-        let register = GearRegister::init(); //TODO: how to get right gear?
-        let add = register.gears.iter().next().unwrap().1;
-        let input = vec![TypedValue::U64(2), TypedValue::U64(2)];
-        let sum = if let &TypedValue::U64(sum) = add.evaluate(input).unwrap().first().unwrap() {
-            sum
-        } else { panic!() };
-        assert_eq!(sum, 4);
-    }
-    */
-
     #[test]
     fn it_compiles() {}
 
