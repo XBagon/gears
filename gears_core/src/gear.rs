90a81c88d8845bab7669b0cec7e9f584871c8dbf
XBagon <xbagon@outlook.de>
2022-09-24 10:51:06 +0200

    composite impl wip

--- gears_core/src/gear.rs
+++ gears_core/src/gear.rs
@@ -1,8 +1,10 @@
-use std::fmt::{Display, Formatter};
-use egg::{EGraph, Id, Language, LanguageChildren, Pattern};
 use derive_more::*;
+use egg::*;
 use petgraph::prelude::*;
 use slotmap::{new_key_type, SlotMap};
+use std::convert::{TryFrom, TryInto};
+use std::fmt::{Display, Formatter};
+use std::ops::Index;
 
 pub struct Gear {
     header: GearHeader,
@@ -10,11 +12,11 @@ pub struct Gear {
 }
 
 impl Gear {
-    pub fn run(&self, inputs: Vec<Value>) -> Result<Vec<Value>> {
+    pub fn run(&self, input: Value) -> Result<Value> {
         //TODO: Are these checks necessary or can this be ensured otherwise?
-        self.header.check_input_types(&inputs)?;
-        let result = self.inner.run(inputs)?;
-        self.header.check_output_types(&result)?;
+        self.header.check_input_type(&input)?;
+        let result = self.inner.run(input)?;
+        self.header.check_output_type(&result)?;
         Ok(result)
     }
 }
@@ -26,19 +28,21 @@ pub struct GearHeader {
 }
 
 impl GearHeader {
-    fn check_input_types(&self, inputs: &Vec<Value>) -> Result<()> {
+    fn check_input_type(&self, input: &Value) -> Result<()> {
+        let input_strct: &Struct = input.to_struct()?;
         self.inputs
             .iter()
-            .zip(inputs)
+            .zip(&*input_strct.0)
             .all(|(header, value)| header.ty == value.ty())
             .then_some(())
             .ok_or(Error::InputTypeMismatch)
     }
 
-    fn check_output_types(&self, outputs: &Vec<Value>) -> Result<()> {
+    fn check_output_type(&self, output: &Value) -> Result<()> {
+        let output_strct: &Struct = output.to_struct()?;
         self.outputs
             .iter()
-            .zip(outputs)
+            .zip(&*output_strct.0)
             .all(|(header, value)| header.ty == value.ty())
             .then_some(())
             .ok_or(Error::OutputTypeMismatch)
@@ -49,6 +53,7 @@ impl GearHeader {
 pub enum Error {
     InputTypeMismatch,
     OutputTypeMismatch,
+    TriedToDestructureNonStruct(Type),
     Unimplemented,
 }
 
@@ -59,44 +64,112 @@ pub struct IOPutHeader {
     ty: Type,
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Type {
     Float,
+    Struct(StructType),
     #[allow(dead_code)]
     Unimplemented,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, From, TryInto)]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct StructType(Box<Vec<Type>>);
+
+#[derive(Clone, Debug, PartialEq, From, TryInto)]
+#[try_into(ref)]
 pub enum Value {
     Float(f32),
+    Struct(Struct),
+    #[allow(dead_code)]
     Unimplemented,
 }
 
 impl Value {
+    pub fn from_vec(vec: Vec<Value>) -> Self {
+        Self::Struct(vec.into())
+    }
+
     fn ty(&self) -> Type {
         match self {
             Value::Float(_) => Type::Float,
+            Value::Struct(strct) => strct.ty(),
             _ => unimplemented!(),
         }
+    }
+
+    pub fn to_struct(&self) -> Result<&Struct> {
+        self.try_into()
+            .map_err(|e| Error::TriedToDestructureNonStruct(self.ty()))
+    }
+
+    pub fn into_struct(self) -> Result<Struct> {
+        let ty = self.ty();
+        self.try_into()
+            .map_err(|e| Error::TriedToDestructureNonStruct(ty))
+    }
+}
+
+impl From<Vec<Value>> for Value {
+    fn from(vec: Vec<Value>) -> Self {
+        Self::from_vec(vec)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Struct(Box<Vec<Value>>);
+
+impl Index<usize> for Struct {
+    type Output = Value;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl Struct {
+    fn ty(&self) -> Type {
+        Type::Struct(StructType(Box::new(
+            self.0.iter().map(|f| f.ty()).collect(),
+        )))
+    }
+}
+
+impl From<Vec<Value>> for Struct {
+    fn from(vec: Vec<Value>) -> Self {
+        Self(Box::new(vec))
+    }
+}
+
+trait WrapInStruct {
+    fn wrap_in_struct(self) -> Struct;
+}
+
+impl WrapInStruct for Vec<Value> {
+    fn wrap_in_struct(self) -> Struct {
+        self.into()
+    }
+}
+
+impl WrapInStruct for Value {
+    fn wrap_in_struct(self) -> Struct {
+        vec![self].into()
     }
 }
 
 new_key_type! {pub struct GearId;}
 
 enum GearInner {
-    RuntimeFunction(fn(Vec<Value>) -> Result<Vec<Value>>),
+    RuntimeFunction(fn(Value) -> Result<Value>),
     Composite(Composite),
     #[allow(dead_code)]
     Unimplemented,
 }
 
 impl GearInner {
-    pub fn run(&self, inputs: Vec<Value>) -> Result<Vec<Value>> {
+    pub fn run(&self, input: Value) -> Result<Value> {
         match self {
-            GearInner::RuntimeFunction(function) => Ok(function(inputs)?),
-            GearInner::Composite(composite) => {
-                composite.run(inputs)
-            }
+            GearInner::RuntimeFunction(function) => Ok(function(input)?),
+            GearInner::Composite(composite) => composite.run(input),
             GearInner::Unimplemented => Err(Error::Unimplemented),
         }
     }
@@ -109,8 +182,59 @@ struct Composite {
 }
 
 impl Composite {
-    pub fn run(&self, inputs: Vec<Value>) -> Result<Vec<Value>> {
-        todo!()
+    pub fn run(&self, input: Value) -> Result<Value> {
+        let mut runtime = Runtime {
+            expr: RecExpr::default(),
+            input,
+            context: &self,
+        };
+
+        let rules = Vec::new();
+        let runner = Runner::default().with_egraph(self.graph.clone()).run(rules); //TODO: use replace_with instead of clone
+        let extractor = Extractor::new(&runner.egraph, AstSize);
+
+        let mut output_vec = Vec::new();
+        for &output in &self.outputs {
+            let (_best_cost, best_expr) = extractor.find_best(output);
+            runtime.expr = best_expr;
+            output_vec.push(runtime.run()?);
+            //panic!("{:?}", best_expr);
+        }
+        Ok(output_vec.into())
+    }
+}
+
+struct Runtime<'a> {
+    context: &'a Composite,
+    expr: RecExpr<GearLanguage>,
+    input: Value,
+}
+
+impl<'a> Runtime<'a> {
+    pub fn run(&self) -> Result<Value> {
+        let top_node = self.expr.as_ref().last().unwrap();
+        self.run_node(&top_node)
+    }
+
+    fn run_node(&self, current_node: &GearLanguage) -> Result<Value> {
+        match current_node {
+            GearLanguage::Destructure(destr) => {
+                let input = self.run_node(&self.expr[destr.child])?;
+                Ok(input.to_struct()?[destr.index].clone())
+            }
+            GearLanguage::Expression(expr) => {
+                let inputs = expr
+                    .children
+                    .iter()
+                    .copied()
+                    .map(|c| self.run_node(&self.expr[c]))
+                    .flatten()
+                    .collect::<Vec<_>>()
+                    .into();
+                self.context.gears[expr.gear].run(inputs)
+            }
+            GearLanguage::In(i) => Ok(self.input.to_struct()?[*i].clone()),
+        }
     }
 }
 
@@ -184,6 +308,16 @@ mod tests {
     use super::*;
     use std::convert::TryInto;
 
+    macro_rules! assert_gear {
+        ($gear:expr, $input:expr, $output:expr) => {{
+            use crate::gear::WrapInStruct;
+            assert_eq!(
+                $gear.run($input.wrap_in_struct().into()).unwrap(),
+                $output.wrap_in_struct().into()
+            );
+        }};
+    }
+
     fn construct_addition_gear() -> Gear {
         Gear {
             header: GearHeader {
@@ -203,10 +337,16 @@ mod tests {
                     ty: Type::Float,
                 }],
             },
-            inner: GearInner::RuntimeFunction(|inputs| {
-                let in0: f32 = inputs[0].try_into().unwrap();
-                let in1: f32 = inputs[1].try_into().unwrap();
-                Ok(vec![(in0 + in1).into()])
+            inner: GearInner::RuntimeFunction(|input| {
+                let mut inputs = *input.into_struct().unwrap().0;
+                let (in0, in1) = if let [in0, in1] = inputs[..] {
+                    (in0, in1)
+                } else {
+                    panic!()
+                };
+                let in0: f32 = in0.try_into().unwrap();
+                let in1: f32 = in1.try_into().unwrap();
+                Ok(vec![Value::Float(in0 + in1)].into())
             }),
         }
     }
@@ -214,18 +354,17 @@ mod tests {
     #[test]
     fn check_addition_gear() {
         let gear = construct_addition_gear();
-        assert!(gear
-            .run(vec![Value::Float(1.0), Value::Float(2.0)])
-            .unwrap()
-            .into_iter()
-            .zip(vec![Value::Float(3.0)])
-            .all(|(actual, expected)| actual == expected));
+        assert_gear!(
+            gear,
+            vec![Value::Float(1.0), Value::Float(2.0)],
+            Value::Float(3.0)
+        );
     }
 
     fn construct_double_gear() -> Gear {
         let mut gears = SlotMap::with_key();
         let addition_gear = gears.insert(construct_addition_gear());
-        let mut graph = EGraph::<GearLanguage,()>::default();
+        let mut graph = EGraph::<GearLanguage, ()>::default();
 
         let input = graph.add(GearLanguage::In(0));
         let addition = graph.add(GearLanguage::Expression(GearExpression {
@@ -241,16 +380,13 @@ mod tests {
 
         graph.dot().to_png("C:/tmp/eggdot.png").unwrap();
 
-
         Gear {
             header: GearHeader {
                 name: String::from("Double"),
-                inputs: vec![
-                    IOPutHeader {
-                        name: String::from("single"),
-                        ty: Type::Float,
-                    },
-                ],
+                inputs: vec![IOPutHeader {
+                    name: String::from("single"),
+                    ty: Type::Float,
+                }],
                 outputs: vec![IOPutHeader {
                     name: String::from("doubled"),
                     ty: Type::Float,
@@ -259,7 +395,7 @@ mod tests {
             inner: GearInner::Composite(Composite {
                 gears,
                 graph,
-                outputs: vec![output]
+                outputs: vec![output],
             }),
         }
     }
@@ -267,11 +403,6 @@ mod tests {
     #[test]
     fn check_double_gear() {
         let gear = construct_double_gear();
-        assert!(gear
-            .run(vec![Value::Float(1.0)])
-            .unwrap()
-            .into_iter()
-            .zip(vec![Value::Float(2.0)])
-            .all(|(actual, expected)| actual == expected));
+        assert_gear!(gear, Value::Float(1.0), Value::Float(2.0))
     }
 }
