0c5033a446c6e338fad551b7e87a79851df39e6d
XBagon <xbagon@outlook.de>
2022-10-02 19:05:53 +0200

    serde tests.. deserializing doesn't work 😬

--- gears_core/src/gear_file.rs
+++ gears_core/src/gear_file.rs
@@ -2,13 +2,13 @@ use std::{
     collections::HashMap,
     fs,
     io::{Read, Write},
-    path::Path,
+    path::{Path, PathBuf},
 };
 
 use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
 
-use crate::gear::Gear;
+use crate::{gear::Gear, gear_file};
 
 const FILE_SIGNATURE: [u8; 8] = *b"\x1F*gears*";
 const CURRENT_VERSION: u32 = 0;
@@ -63,6 +63,7 @@ impl GearFile {
 
         let mut file_bytes = Vec::new();
         file.read_to_end(&mut file_bytes)?;
+        dbg!(&file_bytes);
         let gear_file = postcard::from_bytes(&file_bytes)?;
         Ok(gear_file)
     }
@@ -77,4 +78,48 @@ impl GearFile {
         file.write_all(&file_bytes)?;
         Ok(())
     }
+}
+
+#[test]
+fn ser_de() {
+    use crate::gear::*;
+
+    let gear_file = GearFile::new(
+        MetaData::new(
+            String::from("Test Gear"),
+            String::from("This gear will be tried to be serialized and then deserialized again!"),
+            String::from("ME"),
+            HashMap::from([(String::from("test_tag"), String::from("is important"))]),
+        ),
+        Gear::new(
+            GearHeader {
+                name: String::from("test"),
+                inputs: vec![
+                    IOPutHeader::new(String::from(String::from("some_in")), crate::Type::Float),
+                    IOPutHeader::new(
+                        String::from(String::from("other_in")),
+                        crate::Type::Unimplemented,
+                    ),
+                ],
+                outputs: vec![
+                    IOPutHeader::new(
+                        String::from(String::from("some_out")),
+                        crate::Type::Unimplemented,
+                    ),
+                    IOPutHeader::new(String::from(String::from("other_out")), crate::Type::Float),
+                ],
+            },
+            GearInner::Unimplemented,
+        ),
+    );
+    let bytes = postcard::to_stdvec(&gear_file).unwrap();
+    let mut deserializer = postcard::Deserializer::from_bytes(&bytes);
+    let gear_file: GearFile = serde_path_to_error::deserialize(&mut deserializer).unwrap();
+    //let gear_file: GearFile = postcard::from_bytes(&bytes).unwrap();
+}
+
+#[test]
+fn load_add() {
+    let gear_file = GearFile::read_from_file("../gearify/tests/output/add.gear").unwrap();
+    dbg!(gear_file);
 }
